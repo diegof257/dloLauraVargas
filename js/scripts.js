1db03f977f7d5dcbf6c3f07f6a7990de5799fd1e
diegof257 <diegonzalez.93@gmail.com>
2022-07-26 09:13:29 +0100

    Commit
    Commit libs, img and other pages

---
+++ js/scripts.js
@@ -0,0 +1,68 @@
+var resHtml = "";
+
+function validaBoton(valor,boton){
+
+    var correcto = "Cameron, S. (2021). <i>The business student's handbook: skills for study and employment.</i> 7th ed. Harlow: Pearson.";
+    
+    var input1 = $('#res1').val();
+    var input2 = $("#res2").val();
+    var input3 = $("#res3").val();
+    var input4 = $("#res4").val();
+    var input5 = $("#res5").val();
+    var input6 = $("#res6").val();
+    
+    if(input1 == ""){
+        resHtml += valor+" ";
+        $("#resultado").html(resHtml);
+        $('#'+boton).attr('disabled', true);
+        $('#res1').val(valor);
+
+    }else if(input2 == ""){
+        resHtml += valor+" ";
+        $("#resultado").html(resHtml);
+        $('#'+boton).attr('disabled', true);
+        $('#res2').val(valor); 
+       
+    }else if(input3 == ""){
+        resHtml += valor+" ";
+        $("#resultado").html(resHtml);
+        $('#'+boton).attr('disabled', true);
+        $('#res3').val(valor);
+    }else if(input4 == ""){
+        resHtml += valor+" ";
+        $("#resultado").html(resHtml);
+        $('#'+boton).attr('disabled', true);
+        $('#res4').val(valor);
+    }else if(input5 == ""){
+        resHtml += valor+" ";
+        $("#resultado").html(resHtml);
+        $('#'+boton).attr('disabled', true);
+        $('#res5').val(valor);
+    }else if(input6 == ""){
+        resHtml += valor+"";
+        $("#resultado").html(resHtml);
+        $('#'+boton).attr('disabled', true);
+        $('#res6').val(valor);
+    }
+
+    $("#resultado").html(resHtml);
+
+    if( $('#res6').val() != ""){
+        if(correcto == resHtml){
+            setTimeout(function(){
+                alert("WIN !!");
+           }, 200); 
+           
+        }else{
+            setTimeout(function(){
+                alert("You are a fuking loser !!");
+                for(var i = 1; i <= 6; i++){
+                    $('#btn'+i).attr('disabled', false);
+                    document.getElementById("res"+i).value = "";
+                }
+                $("#resultado").html("");
+                resHtml = "";
+           }, 300);
+        }
+    }
+}
